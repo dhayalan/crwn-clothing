80dc4cdf76d2ed1bfa24c5230de6b974fad7a69d
Dhayalan <dhaijk01@in.ibm.com>
2019-11-01 00:13:37 +0530

    Collection Preview and Collection item added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import { Homepage } from "./pages/homepage.component";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  );
-};
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shoppage/shoppage.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./homepage.styles.scss";
 
-import Directory from "../components/directory/directory.component";
+import Directory from "../../components/directory/directory.component";
 
-export const Homepage = () => {
+const Homepage = () => {
   return (
     <div className="homepage">
       <Directory />
     </div>
   );
 };
+export default Homepage;
