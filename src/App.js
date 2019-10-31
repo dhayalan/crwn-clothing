a3cb141188c34b0e0e6d42ea07e8d4ee6e602114
Dhayalan <dhaijk01@in.ibm.com>
2019-10-31 12:41:11 +0530

    withRouter added to show shop/hats page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import { Homepage } from "./pages/homepage.component";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
